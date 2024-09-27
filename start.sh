c9c83166005bfc230f5043c3adbdcd78e10549e3
LCPGMPA <lcclinicaparagominas@gmail.com>
2024-09-27 16:44:41 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-yarn start
+#!/bin/sh
+
+# Start the wa-automate server
+npx @open-wa/wa-automate --disable-spins --port $PORT --popup --in-docker --qr-timeout 0 --keep-alive --keep-updated --executable-path='/app/.apt/usr/bin/google-chrome' &
+
+# Start the Python app
+python3 app.py
